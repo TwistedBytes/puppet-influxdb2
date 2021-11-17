7de4c53047b49b9d2499bc6e7c33e1332d669445
Derk Gortemaker <derk@gortemaker.nl>
2021-11-17 12:42:14 +0100

    fixes for newer influxdb2

--- lib/facter/influxdb2_version.rb
+++ lib/facter/influxdb2_version.rb
@@ -3,7 +3,7 @@ Facter.add(:influxdb2_version) do
     if Facter::Util::Resolution.which('influxd')
 
       # InfluxDB 2.0.6 (git: 4db98b4c9a) build_date: 2021-04-29T16:48:12Z
-      version_stdout = Facter::Util::Resolution.exec('influx version')
+      version_stdout = Facter::Util::Resolution.exec('influxd version')
       match = version_stdout.match(%r{[0-9]+\.[0-9]+\.[0-9]+})
 
       match ? match[0] : nil
